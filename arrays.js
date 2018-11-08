f06d4055fc88df0e9993e10085a1bf3ce3ca72a8
Steven Wong <stevenwongyt@gmail.com>
2018-11-08 16:20:51 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,5 @@
 var chocolateBars = new Array();
-  chocolateBars = [snickers, hundred grand, kitkat, skittles];
+  chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
   function addElementToBeginningOfArray (array, n) {
     return [n, ... array];
@@ -7,4 +7,33 @@ var chocolateBars = new Array();
 
   function destructivelyAddElementToEndOfArray (array, n) {
     return array.unshift(n);
+  }
+
+  function addElementToEndOfArray (array, n) {
+    return [...array, n];
+  }
+
+  function destructivelyAddElementToEndOfArray (array, n) {
+    return array.push(n);
+  }
+
+  function accessElementInAray (array, i) {
+    return array[i];
+  }
+
+  function destructivelyRemoveElementFromBeginningOfArray (array) {
+    return array.shift();
+  }
+
+  function removeElementFromBeginningOfArray (array) {
+    return array.slice(1);
+  }
+
+  function desctructivelyRemoveElementFromEndOfArray (array) {
+    array.pop();
+    return array;
+  }
+
+  function removeElementFromEndOfArray (array) {
+    return array.slice(0, array.length-1);
   }
